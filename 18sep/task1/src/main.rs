9c5b122a288b192fd92e4f1a5d27bb53f7f06938
Artem Kudyakov <t3m8ch@yandex.ru>
2025-09-25 14:33:13 +0400

    refactor: Move first method to separate function

--- 18sep/task1/src/main.rs
+++ 18sep/task1/src/main.rs
@@ -12,8 +12,8 @@ fn main() {
 
     let n: u32 = n.trim().parse().expect("Failed to parse n");
 
-    let mut x_vector = Vec::new();
-    let mut f_vector = Vec::new();
+    let mut xv_interpolated = Vec::new();
+    let mut fv_interpolated = Vec::new();
 
     for i in 0..=n {
         let mut x = String::new();
@@ -23,7 +23,7 @@ fn main() {
         io::stdin().read_line(&mut x).expect("Failed to read line");
         let x: f64 = x.trim().parse().expect("Failed to parse x");
 
-        x_vector.push(x);
+        xv_interpolated.push(x);
     }
 
     for i in 0..=n {
@@ -34,49 +34,59 @@ fn main() {
         io::stdin().read_line(&mut f).expect("Failed to read line");
         let f: f64 = f.trim().parse().expect("Failed to parse f");
 
-        f_vector.push(f);
+        fv_interpolated.push(f);
     }
 
     println!("\nВходная таблица:");
-    print_table(&x_vector, &f_vector);
+    print_table(&xv_interpolated, &fv_interpolated);
 
-    let x_matrix: Vec<f64> = (0..=n)
-        .into_iter()
-        .map(|i| x_vector.iter().map(move |x| x.powi(i as i32)))
-        .flatten()
-        .collect();
-
-    let x_matrix = DMatrix::from_vec((n + 1) as usize, (n + 1) as usize, x_matrix);
-    let f_vector = DVector::from_vec(f_vector);
-
-    let Some(a_vector) = x_matrix.lu().solve(&f_vector) else {
-        eprintln!("Система не имеет решений!");
-        return;
-    };
-
-    let x_vector: Vec<f64> = [x_vector[0]]
+    let x_interim_vector: Vec<f64> = [xv_interpolated[0]]
         .into_iter()
         .chain(
-            x_vector
+            xv_interpolated
                 .iter()
                 .skip(1)
-                .zip(x_vector.iter())
+                .zip(xv_interpolated.iter())
                 .map(|(x0, x1)| [(x0 + x1) / 2.0, *x0])
                 .flatten(),
         )
         .collect();
 
-    let f_vector: Vec<f64> = x_vector
-        .iter()
+    println!("x_interim_vector: {:#?}", x_interim_vector);
+
+    let f_vector =
+        interpolation_polynomial(n, &xv_interpolated, &fv_interpolated, &x_interim_vector);
+
+    println!("\nВыходная таблица");
+    print_table(&x_interim_vector, &f_vector);
+}
+
+fn interpolation_polynomial(
+    n: u32,
+    xv_interpolated: &[f64],
+    fv_interpolated: &[f64],
+    xv: &[f64],
+) -> Vec<f64> {
+    let x_matrix: Vec<f64> = (0..=n)
+        .into_iter()
+        .map(|i| xv_interpolated.iter().map(move |x| x.powi(i as i32)))
+        .flatten()
+        .collect();
+    let x_matrix = DMatrix::from_vec((n + 1) as usize, (n + 1) as usize, x_matrix);
+    let f_vector = DVector::from_row_slice(fv_interpolated);
+
+    let Some(a_vector) = x_matrix.lu().solve(&f_vector) else {
+        // TODO: Remove panic
+        panic!("Система не имеет решений!");
+    };
+
+    xv.iter()
         .map(|x| {
             (0..=n)
                 .map(|i| x.powi(i as i32) * a_vector[(i as usize, 0)])
                 .sum()
         })
-        .collect();
-
-    println!("\nВыходная таблица");
-    print_table(&x_vector, &f_vector);
+        .collect()
 }
 
 fn print_table(xs: &[f64], fs: &[f64]) {
