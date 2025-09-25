c1c3a742cedd8e5b014e31b8b35cf40f252c7160
Artem Kudyakov <t3m8ch@yandex.ru>
2025-09-26 01:59:20 +0400

    refactor(vandermonde): Move matrix solving to separate function

--- 18sep/task1/src/interp_methods/vandermonde.rs
+++ 18sep/task1/src/interp_methods/vandermonde.rs
@@ -6,30 +6,34 @@ pub fn vandermonde_interpolation(
     f_list_input: &[f64],
     x_list_with_midpoints: &[f64],
 ) -> Vec<f64> {
-    let x_matrix: Vec<f64> = (0..=n)
-        .into_iter()
-        .map(|i| x_list_input.iter().map(move |x| x.powi(i as i32)))
-        .flatten()
-        .collect();
-
-    let x_matrix = DMatrix::from_vec((n + 1) as usize, (n + 1) as usize, x_matrix);
-    let f_vector = DVector::from_row_slice(f_list_input);
-    let a_vector: Vec<f64> = x_matrix
-        .lu()
-        .solve(&f_vector)
-        .expect("Failed to solve")
-        .into_iter()
-        .map(|a| *a)
-        .collect();
-
+    let a_list = solve_matrix(n, x_list_input, f_list_input);
     x_list_with_midpoints
         .iter()
         .map(|x| {
-            a_vector
+            a_list
                 .iter()
                 .enumerate()
                 .map(|(i, a_i)| x.powi(i as i32) * a_i)
                 .sum()
         })
+        .collect()
+}
+
+fn solve_matrix(n: u32, x_list: &[f64], f_list: &[f64]) -> Vec<f64> {
+    let x_matrix: Vec<f64> = (0..=n)
+        .into_iter()
+        .map(|i| x_list.iter().map(move |x| x.powi(i as i32)))
+        .flatten()
+        .collect();
+
+    let x_matrix = DMatrix::from_vec((n + 1) as usize, (n + 1) as usize, x_matrix);
+    let f_vector = DVector::from_row_slice(f_list);
+
+    x_matrix
+        .lu()
+        .solve(&f_vector)
+        .expect("Failed to solve")
+        .into_iter()
+        .map(|a| *a)
         .collect()
 }
