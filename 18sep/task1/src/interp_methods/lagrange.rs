93c2380c838500f142849e120de1384afd06bffc
Artem Kudyakov <t3m8ch@yandex.ru>
2025-09-26 02:33:53 +0400

    refactor(lagrange): Improve readability

--- 18sep/task1/src/interp_methods/lagrange.rs
+++ 18sep/task1/src/interp_methods/lagrange.rs
@@ -6,16 +6,11 @@ pub fn lagrange_interpolation(x_nodes: &[f64], f_nodes: &[f64], x_targets: &[f64
                 .into_iter()
                 .zip(x_nodes)
                 .enumerate()
-                .map(|(f_idx, (fk, xk))| {
-                    let x_list_without_k: Vec<f64> = x_nodes
-                        .iter()
-                        .enumerate()
-                        .filter(|(x_idx, _)| *x_idx != f_idx)
-                        .map(|(_, xi)| *xi)
-                        .collect();
+                .map(|(k, (fk, xk))| {
+                    let x_list_without_k = x_nodes[..k].iter().chain(&x_nodes[k + 1..]);
 
-                    let numerator: f64 = x_list_without_k.iter().map(|xi| x - xi).product();
-                    let denominator: f64 = x_list_without_k.iter().map(|xi| xk - xi).product();
+                    let numerator: f64 = x_list_without_k.clone().map(|xi| x - xi).product();
+                    let denominator: f64 = x_list_without_k.map(|xi| xk - xi).product();
 
                     fk * numerator / denominator
                 })
