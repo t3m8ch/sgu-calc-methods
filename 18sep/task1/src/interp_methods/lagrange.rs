6d65d79e6b23791a692db47b736691a5afad4f21
Artem Kudyakov <t3m8ch@yandex.ru>
2025-09-26 02:17:15 +0400

    refactor(lagrange): Improve readability

--- 18sep/task1/src/interp_methods/lagrange.rs
+++ 18sep/task1/src/interp_methods/lagrange.rs
@@ -1,31 +1,28 @@
 pub fn lagrange_interpolation(
     n: u32,
-    xv_interpolated: &[f64],
-    fv_interpolated: &[f64],
-    xv: &[f64],
+    x_list_input: &[f64],
+    f_list_input: &[f64],
+    x_list_with_midpoints: &[f64],
 ) -> Vec<f64> {
-    xv.iter()
+    x_list_with_midpoints
+        .iter()
         .map(|x| {
-            fv_interpolated
+            f_list_input
                 .into_iter()
-                .zip(xv_interpolated)
+                .zip(x_list_input)
                 .enumerate()
                 .map(|(f_idx, (fk, xk))| {
-                    let xv_interpolated: Vec<f64> = xv_interpolated
+                    let x_list_without_k: Vec<f64> = x_list_input
                         .iter()
                         .enumerate()
                         .filter(|(x_idx, _)| *x_idx != f_idx)
                         .map(|(_, xi)| *xi)
                         .collect();
 
-                    fk * xv_interpolated
-                        .iter()
-                        .map(|xi| x - xi)
-                        .fold(1.0, |acc, m| acc * m)
-                        / xv_interpolated
-                            .iter()
-                            .map(|xi| xk - xi)
-                            .fold(1.0, |acc, m| acc * m)
+                    let numerator: f64 = x_list_without_k.iter().map(|xi| x - xi).product();
+                    let denominator: f64 = x_list_without_k.iter().map(|xi| xk - xi).product();
+
+                    fk * numerator / denominator
                 })
                 .sum()
         })
