3a18fdec6d941f7ed724150de0543b8d00adec4b
Artem Kudyakov <t3m8ch@yandex.ru>
2025-09-26 01:51:47 +0400

    refactor: Improve readability of vandermonde_interpolation

--- 18sep/task1/src/main.rs
+++ 18sep/task1/src/main.rs
@@ -38,23 +38,33 @@ fn x_list_with_midpoints(x_list: &[f64]) -> Vec<f64> {
 
 fn vandermonde_interpolation(
     n: u32,
-    xv_interpolated: &[f64],
-    fv_interpolated: &[f64],
-    xv: &[f64],
+    x_list_input: &[f64],
+    f_list_input: &[f64],
+    x_list_with_midpoints: &[f64],
 ) -> Vec<f64> {
     let x_matrix: Vec<f64> = (0..=n)
         .into_iter()
-        .map(|i| xv_interpolated.iter().map(move |x| x.powi(i as i32)))
+        .map(|i| x_list_input.iter().map(move |x| x.powi(i as i32)))
         .flatten()
         .collect();
-    let x_matrix = DMatrix::from_vec((n + 1) as usize, (n + 1) as usize, x_matrix);
-    let f_vector = DVector::from_row_slice(fv_interpolated);
 
-    let a_vector = x_matrix.lu().solve(&f_vector).expect("Failed to solve");
-    xv.iter()
+    let x_matrix = DMatrix::from_vec((n + 1) as usize, (n + 1) as usize, x_matrix);
+    let f_vector = DVector::from_row_slice(f_list_input);
+    let a_vector: Vec<f64> = x_matrix
+        .lu()
+        .solve(&f_vector)
+        .expect("Failed to solve")
+        .into_iter()
+        .map(|a| *a)
+        .collect();
+
+    x_list_with_midpoints
+        .iter()
         .map(|x| {
-            (0..=n)
-                .map(|i| x.powi(i as i32) * a_vector[(i as usize, 0)])
+            a_vector
+                .iter()
+                .enumerate()
+                .map(|(i, a_i)| x.powi(i as i32) * a_i)
                 .sum()
         })
         .collect()
